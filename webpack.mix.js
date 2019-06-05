b0b08a377110c4434982886cad2329f9ca66de87
Darma Yasa <yasa13darma@gmail.com>
2019-06-05 12:43:10 +0900

    coba minified css dengan purgecss webpack plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,41 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss')
+const tailwindcss = require('tailwindcss');
+const glob = require("glob-all");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 mix.js('src/js/min.js', 'js')
 .sass('src/sass/min.scss', 'css')
   .options({
     processCssUrls: false,
     postCss: [ tailwindcss('./tailwind.config.js') ],
-  })
+})
+
+if (mix.inProduction()) {
+  mix.webpackConfig({
+    plugins: [
+      new PurgecssPlugin({
+
+        // Specify the locations of any files you want to scan for class names.
+        paths: glob.sync([
+          path.join(__dirname, "*.html"),
+//          path.join(__dirname, "resources/js/**/*.vue")
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ["html", "js", "php", "vue"]
+          }
+        ]
+      })
+    ]
+  });
+}
